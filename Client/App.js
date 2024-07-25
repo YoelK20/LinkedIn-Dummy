73181ebd81e1288f82c4bf936ec83843274848cb
YoelK20 <166104713+YoelK20@users.noreply.github.com>
2024-07-25 16:55:14 +0700

    Migrate Stack to Main Stack

--- Client/App.js
+++ Client/App.js
@@ -2,16 +2,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import MainStack from './Stack/MainStack';
 
 const Stack = createNativeStackNavigator()
 export default function App() {
   return (
-    <NavigationContainer>
-
-      <Stack.Navigator initialRouteName='login' screenOptions={{headerShown: false}}>
-        <Stack.Screen name='login' component={LoginScreen}/>
-        <Stack.Screen name='register' component={RegisterScreen}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <MainStack />
   );
 }
